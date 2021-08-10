f3c7c29e7cef21ffc50452cf4c2c8bec3a38aeba
Eric <eric.levinson42@gmail.com>
2021-08-10 09:03:49 -0500

    structure

---
+++ src/pages/AnalysisPage/components/ChartsTabs.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import {
+  MDBTabs,
+  MDBTabsItem,
+  MDBTabsLink,
+  MDBTabsContent,
+  MDBTabsPane,
+  MDBRow,
+  MDBCol,
+  MDBContainer
+} from 'mdb-react-ui-kit';
+import { Table } from '../../../state/ballchasing/ballchasingActions'
+
+export const ChartsTabs = () => {
+  const [verticalActive, setVerticalActive] = useState('tab1');
+
+  const handleVerticalClick = (value: string) => {
+    if (value === verticalActive) {
+      return;
+    }
+
+    setVerticalActive(value);
+  };
+
+  return (
+    <>
+      <MDBRow>
+        <MDBCol size='2'>
+          <MDBTabs className='flex-column text-center'>
+            <MDBTabsItem>
+              <MDBTabsLink onClick={() => handleVerticalClick('tab1')} active={verticalActive === 'tab1'}>
+                Core Stats
+              </MDBTabsLink>
+            </MDBTabsItem>
+            <MDBTabsItem>
+              <MDBTabsLink onClick={() => handleVerticalClick('tab2')} active={verticalActive === 'tab2'}>
+                Boost
+              </MDBTabsLink>
+            </MDBTabsItem>
+            <MDBTabsItem>
+              <MDBTabsLink onClick={() => handleVerticalClick('tab3')} active={verticalActive === 'tab3'}>
+                Positioning
+              </MDBTabsLink>
+            </MDBTabsItem>
+            <MDBTabsItem>
+              <MDBTabsLink onClick={() => handleVerticalClick('tab4')} active={verticalActive === 'tab4'}>
+                Movement
+              </MDBTabsLink>
+            </MDBTabsItem>
+          </MDBTabs>
+        </MDBCol>
+        <MDBCol size='10'>
+          <MDBTabsContent>
+            <MDBTabsPane show={verticalActive === 'tab1'}><MDBContainer className='mx-5'>< Table /></MDBContainer></MDBTabsPane>
+            <MDBTabsPane show={verticalActive === 'tab2'}>Boost</MDBTabsPane>
+            <MDBTabsPane show={verticalActive === 'tab3'}>Positioning</MDBTabsPane>
+            <MDBTabsPane show={verticalActive === 'tab4'}>Movement</MDBTabsPane>
+          </MDBTabsContent>
+        </MDBCol>
+      </MDBRow>
+    </>
+  );
+}
